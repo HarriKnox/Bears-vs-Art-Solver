08163f28876f10d921c91f07cfba9220ad705bc2
HarriKnox <harri.alpha96@gmail.com>
2015-08-15 21:28:23 -0700

    Switched the bit for mathematical trick abuse later

--- Rory.java
+++ Rory.java
@@ -3,12 +3,15 @@ public class Rory
 	private int x;
 	private int y;
 	
-	public static final int UP         = 0b1000;
+	public static final int UP         = 0b0100;
 	public static final int DOWN       = 0b1100;
-	public static final int LEFT       = 0b0010;
+	public static final int LEFT       = 0b0001;
 	public static final int RIGHT      = 0b0011;
 	public static final int UP_LEFT    = UP + LEFT;
 	public static final int UP_RIGHT   = UP + RIGHT;
 	public static final int DOWN_LEFT  = DOWN + LEFT;
 	public static final int DOWN_RIGHT = DOWN + RIGHT;
+	
+	public static final int VERT  = 0b1100;
+	public static final int HORIZ = 0b0011;
 }
