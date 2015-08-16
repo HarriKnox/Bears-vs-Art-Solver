7b51c3ce9f370c748254b09d5ca852d712319a2c
HarriKnox <harri.alpha96@gmail.com>
2015-08-15 21:12:07 -0700

    Added directional constants for direction stuff later

--- Rory.java
+++ Rory.java
@@ -1,4 +1,14 @@
 public class Rory
 {
+	private int x;
+	private int y;
 	
+	public static final int UP         = 0b1000;
+	public static final int DOWN       = 0b1100;
+	public static final int LEFT       = 0b0010;
+	public static final int RIGHT      = 0b0011;
+	public static final int UP_LEFT    = UP + LEFT;
+	public static final int UP_RIGHT   = UP + RIGHT;
+	public static final int DOWN_LEFT  = DOWN + LEFT;
+	public static final int DOWN_RIGHT = DOWN + RIGHT;
 }
