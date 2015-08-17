b557814c4ffc38b61193cd5dd96c54237b48c37d
HarriKnox <harri.alpha96@gmail.com>
2015-08-16 17:38:35 -0700

    Moved Rory directions into Directions class

---
+++ Directions.java
@@ -0,0 +1,14 @@
+public final class Directions
+{
+	public static final int UP         = 0b0100;
+	public static final int DOWN       = 0b1100;
+	public static final int LEFT       = 0b0001;
+	public static final int RIGHT      = 0b0011;
+	public static final int UP_LEFT    = UP + LEFT;
+	public static final int UP_RIGHT   = UP + RIGHT;
+	public static final int DOWN_LEFT  = DOWN + LEFT;
+	public static final int DOWN_RIGHT = DOWN + RIGHT;
+	
+	public static final int VERT  = 0b1100;
+	public static final int HORIZ = 0b0011;
+}
--- Rory.java
+++ Rory.java
@@ -1,17 +1,13 @@
 public class Rory
 {
-	private int x;
-	private int y;
+	private int row;
+	private int col;
 	
-	public static final int UP         = 0b0100;
-	public static final int DOWN       = 0b1100;
-	public static final int LEFT       = 0b0001;
-	public static final int RIGHT      = 0b0011;
-	public static final int UP_LEFT    = UP + LEFT;
-	public static final int UP_RIGHT   = UP + RIGHT;
-	public static final int DOWN_LEFT  = DOWN + LEFT;
-	public static final int DOWN_RIGHT = DOWN + RIGHT;
+	public Rory(int row, int col)
+	{
+		this.row = row;
+		this.col = col;
+	}
 	
-	public static final int VERT  = 0b1100;
-	public static final int HORIZ = 0b0011;
+	
 }
