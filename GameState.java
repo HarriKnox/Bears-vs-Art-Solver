37c5c6dc5e6cd47e98bb5e2b222c2c788ba53bf0
HarriKnox <harri.alpha96@gmail.com>
2015-08-18 01:42:41 -0700

    Aside from the main() method, minor changes for functionality in the code

--- GameState.java
+++ GameState.java
@@ -2,14 +2,16 @@ import java.util.LinkedList;
 
 public class GameState
 {
-	private Grid<GridSpace> gameBoard;
-	private int roryRow;
-	private int roryCol;
+	public Grid<GridSpace> gameBoard;
+	public int roryRow;
+	public int roryCol;
+	
+	
 	
 	private int[] getDirections()
 	{
 		LinkedList<Integer> dirList = new LinkedList<>();
-		for (int d = 0; d < Directions.LIST; d++)
+		for (int d = 0, len = Directions.LIST.length; d < len; d++)
 		{
 			int dir = Directions.LIST[d];
 			if (canGo(dir)) dirList.add(Integer.valueOf(dir));
@@ -20,9 +22,7 @@ public class GameState
 	
 	private boolean canGo(int dir)
 	{
-		return (Directions.isVertical(dir) && (this.checkDir(Directions.verticalComponent(dir)) && (!Directions.isHorizontal(dir) || (this.checkDir(Directions.horizontalComponent(dir)) && this.checkDir(dir))))) || (Directions.isHorizontal(dir) && this.checkDir(Directions.horizontalComponent(dir)));
-		
-		/*if (Directions.isVertical(dir))
+		if (Directions.isVertical(dir))
 		{
 			boolean vert = this.checkDir(Directions.verticalComponent(dir));
 			if (Directions.isHorizontal(dir)) return vert && this.checkDir(Directions.horizontalComponent(dir)) && this.checkDir(dir);
@@ -32,13 +32,26 @@ public class GameState
 		{
 			return this.checkDir(Directions.horizontalComponent(dir));
 		}
-		return false;*/
+		return false;
 	}
 	
 	private boolean checkDir(int dir)
 	{
-		int x = roryRow + Directions.verticalChange(dir);
-		int y = roryCol + Directions.horizontalChange(dir);
-		return this.gameBoard.inRange(x, y) && !this.gameBoard.get(x, y).isSolid()
+		int x = this.roryRow + Directions.verticalChange(dir);
+		int y = this.roryCol + Directions.horizontalChange(dir);
+		return this.gameBoard.inRange(x, y) && !this.gameBoard.get(x, y).isSolid();
+	}
+	
+	public static void main(String[] args)
+	{
+		GameState gs = new GameState();
+		gs.gameBoard = new Grid<GridSpace>(5, 5, (Integer x, Integer y) -> new GridSpace.Wall(x, y));
+		gs.roryRow = 2;
+		gs.roryCol = 2;
+		for (int x = 1; x < 4; x++)
+			for (int y = 1; y < 4; y++)
+				gs.gameBoard.set(x, y, new GridSpace.Space(x, y));
+		gs.gameBoard.set(1, 1, new GridSpace.Wall(2, 1));
+		System.out.println(java.util.Arrays.toString(java.util.Arrays.stream(gs.getDirections()).mapToObj((int i) -> Directions.NAMES[i]).toArray()));
 	}
 }
