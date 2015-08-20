30450d5fb228894e5d658caa376e2e7b18483335
HarriKnox <harri.alpha96@gmail.com>
2015-08-19 20:27:07 -0700

    Everything now overrides equals and hashCode. Also, added hasArt.

--- GridSpace.java
+++ GridSpace.java
@@ -2,22 +2,30 @@ public abstract class GridSpace
 {
 	private static final GridSpace WALL = new Wall();
 	
-	public boolean isSolid() { return false; }
-	public abstract GridSpace copy();
-	
-	public static GridSpace getSpace() { return new Space(); }
 	public static GridSpace getWall() { return WALL; }
+	public static GridSpace getSpace() { return new Space(); }
 	
-	private static final class Space extends GridSpace
-	{
-		public GridSpace copy() { return new Space(); }
-		public String toString() { return " "; }
-	}
+	private boolean art = false;
+	public boolean hasArt() { return this.art; }
+	public abstract boolean isSolid();
+	public abstract GridSpace copy();
 	
 	private static final class Wall extends GridSpace
 	{
+		public boolean hasArt() { return false; }
 		public boolean isSolid() { return true; }
 		public GridSpace copy() { return this; }
 		public String toString() { return "W"; }
+		public int hashCode() { return 0b0001; }
+		public boolean equals(Object o) { return o instanceof Wall; }
+	}
+	
+	private static final class Space extends GridSpace
+	{
+		public boolean isSolid() { return false; }
+		public GridSpace copy() { return new Space(); }
+		public String toString() { return " "; }
+		public int hashCode() { return 0b0010 + (this.hasArt() ? 0b10000 : 0); }
+		public boolean equals(Object o) { return o instanceof Space && ((Space)o.hasArt() == this.hasArt()); }
 	}
 }
