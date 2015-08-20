c9b0d79e60b15cbf8df37a5c52cc7f49db848533
HarriKnox <harri.alpha96@gmail.com>
2015-08-20 14:08:01 -0700

    Added a few abstract methods

--- GridSpace.java
+++ GridSpace.java
@@ -5,31 +5,41 @@ public abstract class GridSpace
 	public static GridSpace getWall() { return WALL; }
 	public static GridSpace getSpace() { return new Space(); }
 	
+	
 	private boolean art = false;
 	public boolean hasArt() { return this.art; }
 	public boolean setArt() { return this.art = true; }
+	
 	public abstract boolean isSolid();
 	public abstract GridSpace copy();
-	public abstract String toString();
-	public int hashCode() { return (this.art ? 0b100000 : 0); }
+	protected final GridSpace copy(GridSpace gs) { gs.art = this.art; return gs; }
+	public void passOver() {;}
+	public void endOfMove() {;}
+	public void landedOn() { this.art = false; }
+	
+	protected abstract int ID();
+	private int laserArtHash() { return (this.art ? 1 << 4 : 0); }
+	protected int metadataHash() { return 0; }
+	public final int hashCode() { return this.metadataHash() + this.laserArtHash() + this.ID(); }
 	public boolean equals(Object o) { return o instanceof GridSpace && (((GridSpace)o).art == this.art); }
+	public abstract String toString();
 	
 	private static final class Wall extends GridSpace
 	{
-		public boolean hasArt() { return false; }
+		protected int ID() { return 1; }
+		public boolean setArt() { return false; }
 		public boolean isSolid() { return true; }
 		public GridSpace copy() { return this; }
 		public String toString() { return "W"; }
-		public int hashCode() { return 0b0001; }
 		public boolean equals(Object o) { return o instanceof Wall; }
 	}
 	
 	private static final class Space extends GridSpace
 	{
+		protected int ID() { return 2; }
 		public boolean isSolid() { return false; }
-		public GridSpace copy() { return new Space(); }
+		public GridSpace copy() { return super.copy(new Space()); }
 		public String toString() { return " "; }
-		public int hashCode() { return 0b0010 + super.hashCode(); }
 		public boolean equals(Object o) { return o instanceof Space && super.equals(o); }
 	}
 }
