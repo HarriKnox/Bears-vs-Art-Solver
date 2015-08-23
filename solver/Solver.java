feb1f29bb8d92b4cdb4e04e01d029ae0a0fdf234
HarriKnox <harri.alpha96@gmail.com>
2015-08-22 19:47:16 -0700

    Added the solve() method to go through and solve the puzzle

--- solver/Solver.java
+++ solver/Solver.java
@@ -9,44 +9,56 @@ import utility.*;
 
 public class Solver
 {
-	private LinkedList<GameState> openQueue = new LinkedList<>();
-	private HashSet<GameState> closedSet = new HashSet<>(150);
+	private LinkedList<GameState> openQueue;
+	private HashSet<GameState> closedSet;
+	
+	private int maxMoves;
+	
+	public Solver(Grid<GridSpace> grid, int row, int col, int max)
+	{
+		int gridSize = grid.size();
+		this.openQueue = new LinkedList<>();
+		this.closedSet = new HashSet<>((int)Math.pow(gridSize, 3));
+		this.addState(new GameState(grid, row, col));
+		this.maxMoves = max;
+	}
 	
 	private boolean addState(GameState state)
 	{
 		if (!this.closedSet.contains(state))
 		{
-			this.openQueue.add(state);
+			this.openQueue.addLast(state);
 			this.closedSet.add(state);
 			return true;
 		}
 		return false;
 	}
 	
-	private boolean addStates(GameState[] states)
+	public int[] solve()
 	{
-		boolean added = false;
-		for (int s = 0, len = states.length; s < len; s++)
-			added = added | this.addState(states[s]);
-		return added;
-	}
-	
-	private boolean processFirstState()
-	{
-		if (this.openQueue.size() > 0)
+		while (!this.openQueue.isEmpty())
 		{
-			GameState state = this.openQueue.pollFirst();
-			return this.addStates(state.createResultingGameStates());
+			GameState[] states = this.openQueue.removeFirst().createResultingGameStates();
+			for (int s = 0, len = states.length; s < len; s++)
+			{
+				GameState state = states[s];
+				if (state.stillAlive(this.maxMoves))
+				{
+					if (state.success()) return state.getDirections();
+					
+					this.addState(state);
+				}
+			}
 		}
-		return false;
+		return new int[0];
 	}
 	
-	private String sizes()
+	public String sizes()
 	{
 		return (new StringBuilder("Open: ")).append(this.openQueue.size()).append(", Closed: ").append(this.closedSet.size()).toString();
 	}
 	
-	private static String solution(int[] dirs)
+	public static String solution(int[] dirs)
 	{
 		return (new StringBuilder(GameState.dirsToString(dirs))).append(' ').append(dirs.length).toString();
 	}
