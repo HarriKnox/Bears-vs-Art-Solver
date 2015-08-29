19989a3d0615a30bf2f626d2ac626b69454ea62a
HarriKnox <harri.alpha96@gmail.com>
2015-08-29 14:36:32 -0700

    Added everything to Spike

--- gridspace/Spike.java
+++ gridspace/Spike.java
@@ -1,8 +1,24 @@
-package gridspace
+package gridspace;
 
 import solver.GameState;
 
-final class Spike extends GridSpace
+public final class Spike extends GridSpace
 {
+	private boolean up = false;
+	public boolean setUp(boolean up) { return this.up = up; }
 	
+	protected int ID() { return 2; }
+	protected int metadataHash() { return b2i(this.up); }
+	
+	public String toString() { return super.toString(this.up ? 'S' : 's'); }
+	
+	public void checkHazard(GameState state) { if (this.up) state.kill(); else super.checkHazard(state); }
+	public void endOfMove(GameState state) { this.up = !this.up; }
+	
+	public GridSpace copy()
+	{
+		Spike s = new Spike();
+		s.up = this.up;
+		return super.copy(s);
+	}
 }
