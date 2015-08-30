7236e54e0e2ba36610264ac7347cfe87e32e63ef
HarriKnox <harri.alpha96@gmail.com>
2015-08-29 22:45:05 -0700

    Removed methods and made protected fields package-private

--- gridspace/GridSpace.java
+++ gridspace/GridSpace.java
@@ -12,27 +12,15 @@ public abstract class GridSpace
 	public static final GridSpace getSpike() { return new Spike(); }
 	
 	
-	private boolean art = false;
-	public boolean hasArt() { return this.art; }
-	public boolean setArt(boolean has) { return this.art = has; }
-	
-	private boolean laser = false;
-	public boolean hasLaser() { return this.laser; }
-	public boolean setLaser(boolean las) { return this.laser = las; }
-	
-	private int laserSource = Directions.NONE;
-	private boolean laserBlue = false;
-	private boolean laserOn = false;
-	public int laserSourceDirection() { return this.laserSource; }
-	public int setLaserSource(int dir) { return this.laserSource = dir; }
-	public boolean setLaserBlue(boolean blue) { return this.laserBlue = blue; }
-	public boolean isLaserSource() { return Directions.isDir(this.laserSource); }
-	public boolean laserSourceOn() { return this.laserOn; }
-	public boolean setLaserSourceOn(boolean on) { return this.laserOn = on; }
+	boolean art = false;
+	boolean laser = false;
+	int laserSourceDirection = Directions.NONE;
+	boolean laserSourceBlue = false;
+	boolean laserSourceOn = false;
 	
 	private final int laserArtHash()
 	{
-		return (this.laserSource << 4) +
+		return (this.laserSourceDirection << 4) +
 		       (b2i(this.laserBlue) << 3) +
 		       (b2i(this.laserOn) << 2) +
 		       (b2i(this.laser) << 1) +
@@ -41,7 +29,7 @@ public abstract class GridSpace
 	
 	
 	public abstract String toString();
-	protected String toString(char l)
+	String toString(char l)
 	{
 		StringBuilder sb = new StringBuilder();
 		if (this.laser) sb.append("\033[41m");
@@ -68,22 +56,22 @@ public abstract class GridSpace
 	}
 	
 	
-	protected abstract int ID();
-	protected abstract int metadataHash();
+	abstract int ID();
+	abstract int metadataHash();
 	
 	public abstract GridSpace copy();
 	final GridSpace copy(GridSpace gs)
 	{
 		gs.art = this.art;
 		gs.laser = this.laser;
-		gs.laserOn = this.laserOn;
-		gs.laserBlue = this.laserBlue;
-		gs.laserSource = this.laserSource;
+		gs.laserSourceOn = this.laserSourceOn;
+		gs.laserSourceBlue = this.laserSourceBlue;
+		gs.laserSourceDirection = this.laserSourceDirection;
 		return gs;
 	}
 	
 	public final int hashCode() { return (this.metadataHash() << 12) + (this.laserArtHash() << 4) + this.ID(); }
 	public final boolean equals(Object that) { return (that instanceof GridSpace) && (this.hashCode() == that.hashCode()); }
 	
-	protected static final int b2i(boolean thing) { return thing ? 1 : 0; }
+	static final int b2i(boolean thing) { return thing ? 1 : 0; }
 }
