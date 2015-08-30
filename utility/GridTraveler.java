846d2c2aa5e989543bd46a7609503b098a0275d6
HarriKnox <harri.alpha96@gmail.com>
2015-08-30 12:10:43 -0700

    Added checkDir

--- utility/GridTraveler.java
+++ utility/GridTraveler.java
@@ -1,4 +1,13 @@
+package gridspace;
+
 public class GridTraveler
 {
 	
+	
+	public static boolean checkDir(Grid<GridSpace> gameBoard, int x, int y, int dir)
+	{
+		x += Directions.verticalChange(dir);
+		y += Directions.horizontalChange(dir);
+		return gameBoard.inRange(x, y) && !gameBoard.get(x, y).isSolid();
+	}
 }
