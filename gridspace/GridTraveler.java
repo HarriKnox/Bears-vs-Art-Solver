ff19b9950962d27799de2a4977922d4218f19782
HarriKnox <harri.alpha96@gmail.com>
2015-08-30 12:14:00 -0700

    Added canGo

--- gridspace/GridTraveler.java
+++ gridspace/GridTraveler.java
@@ -4,10 +4,25 @@ public class GridTraveler
 {
 	
 	
-	public static boolean checkDir(Grid<GridSpace> gameBoard, int x, int y, int dir)
+	public static boolean canGo(Grid<GridSpace> gameBoard, int row, int col, int dir)
 	{
-		x += Directions.verticalChange(dir);
-		y += Directions.horizontalChange(dir);
-		return gameBoard.inRange(x, y) && !gameBoard.get(x, y).isSolid();
+		if (Directions.isVertical(dir))
+		{
+			boolean vert = checkDir(gameBoard, row, col, Directions.verticalComponent(dir));
+			if (Directions.isHorizontal(dir)) return vert && checkDir(gameBoard, row, col, Directions.horizontalComponent(dir)) && checkDir(gameBoard, row, col, dir);
+			return vert;
+		}
+		else if (Directions.isHorizontal(dir))
+		{
+			return checkDir(gameBoard, row, col, Directions.horizontalComponent(dir));
+		}
+		return false;
+	}
+	
+	public static boolean checkDir(Grid<GridSpace> gameBoard, int row, int col, int dir)
+	{
+		row += Directions.verticalChange(dir);
+		col += Directions.horizontalChange(dir);
+		return gameBoard.inRange(row, col) && !gameBoard.get(row, col).isSolid();
 	}
 }
