d7cd71e5f5b9f1ba729740a4cf083de37c46843d
HarriKnox <harri.alpha96@gmail.com>
2015-08-30 12:16:58 -0700

    Added copyGrid

--- gridspace/GridLiaison.java
+++ gridspace/GridLiaison.java
@@ -17,4 +17,9 @@ public final class GridLiaison
 	
 	public void ensureCapacity(int rows, int cols) { this.grid.ensureCapacity(rows, cols); }
 	public void trim(int rows, int cols) { this.grid.trim(rows, cols); }
+	
+	public static Grid<GridSpace> copyGrid(Grid<GridSpace> grid)
+	{
+		return new Grid<GridSpace>(grid.rows(), gridcols(), (Integer x, Integer y) -> grid.get(x, y).copy());
+	}
 }
