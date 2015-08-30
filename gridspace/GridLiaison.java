52cc6d4c8312e9c896c58bc8af95d3b7a0772aef
HarriKnox <harri.alpha96@gmail.com>
2015-08-29 22:44:44 -0700

    Added methods to access information about the grid and affect its size

--- gridspace/GridLiaison.java
+++ gridspace/GridLiaison.java
@@ -4,5 +4,17 @@ import utility.*;
 
 public class GridLiaison
 {
+	private Grid<GridSpace> grid;
 	
+	public GridLiaison(int rows, int cols)
+	{
+		this.grid = new Grid(rows, cols, GridSpace.getWall());
+	}
+	
+	public int size() { return this.grid.size(); }
+	public int rows() { return this.grid.rows(); }
+	public int cols() { return this.grid.cols(); }
+	
+	public void ensureCapacity(int rows, int cols) { this.grid.ensureCapacity(rows, cols); }
+	public void trim(int rows, int cols) { this.grid.trim(rows, cols); }
 }
