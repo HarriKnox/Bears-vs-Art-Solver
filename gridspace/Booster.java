8ab5602d2a98b5846b4759cb2203572cdf6a24c7
HarriKnox <harri.alpha96@gmail.com>
2015-09-04 04:17:28 -0400

    Added metadataHash to Booster

--- gridspace/Booster.java
+++ gridspace/Booster.java
@@ -5,5 +5,14 @@ import utility.*
 
 final class Booster extends GridSpace
 {
+	int direction = Directions.RIGHT;
+	boolean rotates = false;
+	boolean clockwise = true;
 	
+	int metadataHash()
+    {
+		return (this.direction << 2) +
+		       (this.rotates ? (b2i(this.clockwise) << 1) + 1 : 0);
+		// I shortcutted right here since this.rotates is already true, so it would always be a 1 in that spot.
+    }
 }
