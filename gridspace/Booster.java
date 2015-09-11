60e83d4b3e1273d0d5c6317c86000660bc74497b
HarriKnox <harri.alpha96@gmail.com>
2015-09-11 15:05:11 +0100

    Added toString and passThrough to Booster.

--- gridspace/Booster.java
+++ gridspace/Booster.java
@@ -10,9 +10,19 @@ final class Booster extends GridSpace
 	boolean clockwise = true;
 	
 	int metadataHash()
-    {
+	{
 		return (this.direction << 2) +
 		       (this.rotates ? (b2i(this.clockwise) << 1) + 1 : 0);
 		// I shortcutted right here since this.rotates is already true, so it would always be a 1 in that spot.
-    }
+	}
+	
+	public String toString()
+	{
+		return super.toString('b');
+	}
+
+	public void passThrough(GameState state)
+	{
+		state.changeDirection(this.direction);
+	}
 }
