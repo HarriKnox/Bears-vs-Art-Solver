a8ed7d5efc626795837491bc3ebe6b6fd3951923
HarriKnox <harri.alpha96@gmail.com>
2015-09-16 14:45:22 -0700

    Added stuff for ButtonDoor: fields, makeCopy, and isSolid

--- gridspace/ButtonDoor.java
+++ gridspace/ButtonDoor.java
@@ -4,5 +4,18 @@ import utility.Colors;
 
 final class ButtonDoor extends GridSpace
 {
+	ButtonDoor() {}
 	
+	boolean up = true;
+	int color = Colors.RED;
+	
+	public boolean isSolid() { return this.up; }
+	
+	GridSpace makeCopy()
+	{
+		ButtonDoor bd = new ButtonDoor();
+		bd.up = this.up;
+		bd.color = this.color;
+		return bd;
+	}
 }
