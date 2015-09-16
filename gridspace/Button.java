374897fdfe44d1e0de101fe1501c9befcef9dee7
HarriKnox <harri.alpha96@gmail.com>
2015-09-16 11:23:42 -0700

    Added passThrough() and makeCopy()

--- gridspace/Button.java
+++ gridspace/Button.java
@@ -6,4 +6,30 @@ import utility.*;
 final class Button extends GridSpace
 {
 	Button() {}
+	
+	boolean up = true;
+	int color = Colors.RED;
+	
+	public void passThrough(GameState state)
+	{
+		if (this.up)
+		{
+			for (GridSpace gs : state.getGameBoard())
+			{
+				if (gs instanceof Button)
+				{
+					Button b = (Button)gs;
+					if (b.color == this.color) b.up = !b.up;
+				}
+			}
+		}
+	}
+	
+	GridSpace makeCopy()
+	{
+		Button b = new Button();
+		b.up = this.up;
+		b.color = this.color;
+		return b;
+	}
 }
