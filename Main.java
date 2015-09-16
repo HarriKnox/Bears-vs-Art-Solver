1b166b0afada407248bc58997562b8c33bae1730
HarriKnox <harri.alpha96@gmail.com>
2015-09-16 16:34:20 -0700

    Changed and added a few more levels for testing

--- Main.java
+++ Main.java
@@ -31,7 +31,7 @@ public class Main //extends PApplet
 	{
 		//PApplet.main("Main");
 		
-		String[] board = {
+		/*String[] board = {
 			"ww ww",
 			"ww ww",
 			"R b a",
@@ -65,9 +65,54 @@ public class Main //extends PApplet
 		liaison.setRotates(5, 2, true);
 		liaison.setClockwise(5, 2, false);
 		
-		liaison.setDirection(7, 2, Directions.UP);
+		liaison.setDirection(7, 2, Directions.UP);/* */
 		
-		Solver solver = new Solver(liaison, 7);
+		
+		/*GridLiaison liaison = new GridLiaison(5, 4, 0, 1);
+		
+		liaison.setCell(0, 0, GridSpace.BUTTON_DOOR);
+		for (int i = 0; i < 3; i++) liaison.setCell(1, i, GridSpace.BUTTON_DOOR);
+		for (int i = 2; i < 4; i++) liaison.setCell(3, i, GridSpace.BUTTON_DOOR);
+		liaison.setCell(4, 3, GridSpace.BUTTON_DOOR);
+		
+		liaison.setCell(4, 0, GridSpace.BUTTON);
+		
+		liaison.setArt(1, 0, true);
+		liaison.setArt(0, 2, true);
+		liaison.setArt(3, 3, true);
+		
+		System.out.println(liaison.copyGrid());/* */
+		
+		
+		GridLiaison liaison = new GridLiaison(5, 5, 2, 2);
+		
+		liaison.setCell(1, 2, GridSpace.BUTTON_DOOR);
+		liaison.setCell(2, 1, GridSpace.BUTTON_DOOR);
+		liaison.setCell(2, 3, GridSpace.BUTTON_DOOR);
+		liaison.setCell(3, 2, GridSpace.BUTTON_DOOR);
+		
+		liaison.setUp(1, 2, false);
+		liaison.setUp(2, 1, false);
+		liaison.setUp(2, 3, false);
+		liaison.setUp(3, 2, false);
+		
+		liaison.setCell(0, 1, GridSpace.BUTTON);
+		liaison.setArt(0, 1, true);
+		liaison.setUp(0, 1, false);
+		
+		liaison.setCell(1, 4, GridSpace.BUTTON);
+		liaison.setArt(1, 4, true);
+		
+		liaison.setCell(4, 3, GridSpace.BUTTON);
+		liaison.setArt(4, 3, true);
+		liaison.setUp(4, 3, false);
+		
+		liaison.setCell(3, 0, GridSpace.BUTTON);
+		liaison.setArt(3, 0, true);
+		
+		System.out.println(liaison.copyGrid());
+		
+		Solver solver = new Solver(liaison, 30);
 		
 		//System.out.println(solver.first().gameBoard);
 		System.out.println(Solver.solution(solver.solve(0)));
