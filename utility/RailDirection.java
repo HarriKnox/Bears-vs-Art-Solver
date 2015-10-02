09260499b6437498ce0b724571d89867147db7c9
HarriKnox <harri.alpha96@gmail.com>
2015-10-02 11:34:24 -0700

    Changes of RailDirection: enum value declarations use Direction values instead of the int values; MAP contains every combination so it's simple to get any combination of rail directions without needing to observe direction order precedence; added private methods to calculate hashes and such; simple reorganization

--- utility/RailDirection.java
+++ utility/RailDirection.java
@@ -2,38 +2,50 @@ package utility;
 
 public enum RailDirection
 {
-	UP_UP      (UP, UP),
-	UP_DOWN    (UP, DOWN),
-	UP_LEFT    (UP, LEFT),
-	UP_RIGHT   (UP, RIGHT),
-	DOWN_DOWN  (DOWN, DOWN),
-	DOWN_LEFT  (DOWN, LEFT),
-	DOWN_RIGHT (DOWN, RIGHT),
-	LEFT_LEFT  (LEFT, LEFT),
-	LEFT_RIGHT (LEFT, RIGHT),
-	RIGHT_RIGHT(RIGHT, RIGHT);
+	UP_UP      (Direction.UP,    Direction.UP),
+	UP_DOWN    (Direction.UP,    Direction.DOWN),
+	UP_LEFT    (Direction.UP,    Direction.LEFT),
+	UP_RIGHT   (Direction.UP,    Direction.RIGHT),
+	DOWN_DOWN  (Direction.DOWN,  Direction.DOWN),
+	DOWN_LEFT  (Direction.DOWN,  Direction.LEFT),
+	DOWN_RIGHT (Direction.DOWN,  Direction.RIGHT),
+	LEFT_LEFT  (Direction.LEFT,  Direction.LEFT),
+	LEFT_RIGHT (Direction.LEFT,  Direction.RIGHT),
+	RIGHT_RIGHT(Direction.RIGHT, Direction.RIGHT);
 	
-	private static final int LEFT = 0b00;
-	private static final int RIGHT = 0b01;
+	
+	public final int hash;
+	private RailDirection(Direction first, Direction last) { this.hash = getHash(first, last); }
+	
 	private static final int UP = 0b10;
 	private static final int DOWN = 0b11;
+	private static final int LEFT = 0b00;
+	private static final int RIGHT = 0b01;
 	
 	private static final RailDirection[] MAP = new RailDirection[16];
 	static
 	{
-		MAP[UP_UP]       = UP_UP;
-		MAP[UP_DOWN]     = UP_DOWN;
-		MAP[UP_LEFT]     = UP_LEFT;
-		MAP[UP_RIGHT]    = UP_RIGHT;
-		MAP[DOWN_DOWN]   = DOWN_DOWN;
-		MAP[DOWN_LEFT]   = DOWN_LEFT;
-		MAP[DOWN_RIGHT]  = DOWN_RIGHT;
-		MAP[LEFT_LEFT]   = LEFT_LEFT;
-		MAP[LEFT_RIGHT]  = LEFT_RIGHT;
-		MAP[RIGHT_RIGHT] = RIGHT_RIGHT;
+		MAP[add(UP, UP)]       = UP_UP;
+		MAP[add(UP, DOWN)]     = UP_DOWN;
+		MAP[add(UP, LEFT)]     = UP_LEFT;
+		MAP[add(UP, RIGHT)]    = UP_RIGHT;
+		MAP[add(DOWN, UP)]     = UP_DOWN;
+		MAP[add(DOWN, DOWN)]   = DOWN_DOWN;
+		MAP[add(DOWN, LEFT)]   = DOWN_LEFT;
+		MAP[add(DOWN, RIGHT)]  = DOWN_RIGHT;
+		MAP[add(LEFT, UP)]     = UP_LEFT;
+		MAP[add(LEFT, DOWN)]   = DOWN_LEFT;
+		MAP[add(LEFT, LEFT)]   = LEFT_LEFT;
+		MAP[add(LEFT, RIGHT)]  = LEFT_RIGHT;
+		MAP[add(RIGHT, UP)]    = UP_RIGHT;
+		MAP[add(RIGHT, DOWN)]  = DOWN_RIGHT;
+		MAP[add(RIGHT, LEFT)]  = LEFT_RIGHT;
+		MAP[add(RIGHT, RIGHT)] = RIGHT_RIGHT;
 	}
 	
-	public final int hash;
+	public static RailDirection get(Direction first, Direction last) { return MAP[add(convert(first), convert(last))]; }
 	
-	private RailDirection(int first, int last) { this.hash = (first << 2) + last; }
+	private static int convert(Direction dir) { return dir.isVertical() ? (dir.verticalComponent() == Direction.UP ? UP : DOWN) : (dir.horizontalComponent() == Direction.LEFT ? LEFT : RIGHT); }
+	private static int add(int first, int last) { return (first << 2) + last; }
+	private static int getHash(Direction first, Direction last) { return add(convert(first), convert(last)); }
 }
