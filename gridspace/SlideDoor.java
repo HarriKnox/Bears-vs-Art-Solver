074173386389a34c80971a5ce1b01cc218cdb560
HarriKnox <harri.alpha96@gmail.com>
2015-10-05 01:19:24 -0700

    Added default values

--- gridspace/SlideDoor.java
+++ gridspace/SlideDoor.java
@@ -4,7 +4,7 @@ import utility.*;
 
 final class SlideDoor extends GridSpace
 {
-	RailDirection rail;
-	Direction heading;
-	boolean up;
+	RailDirection rail = RailDirection.LEFT_RIGHT;
+	Direction heading = Direction.RIGHT;
+	boolean up = true;
 }
