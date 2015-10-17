1eb1f601a2f31af50ce3ba6c0a3f4faa55d5ecfb
HarriKnox <harri.alpha96@gmail.com>
2015-10-17 15:12:22 -0700

    Made a ton of changes, mostly rearranging the various functions and refactoring. Added monoSlideDoors and changed slideDoors to upSlideDoors. monoSlideDoors contains all SlideDoors that are monodirectional. upSlideDoors contains all SlideDoors that are up.

--- gridspace/GridChecker.java
+++ gridspace/GridChecker.java
@@ -23,8 +23,8 @@ public final class GridChecker
 		(6) boosters that point directly into a potentially solid block (boosters
 			can't point into solid blocks, and even if it isn't solid yet,
 			it may be solid later)
-		(7) portals of the same length having a mismatch in entry-exit directions
-		(8) 1 or 3+ portals of a color (can have only 0 or 2)
+		(7) this.portals of the same length having a mismatch in entry-exit directions
+		(8) 1 or 3+ this.portals of a color (can have only 0 or 2)
 		
 		@todo Add the following
 		(9) rails that are cycles have all sliding blocks going the same direction
@@ -32,90 +32,74 @@ public final class GridChecker
 	**/
 	public static void checkGrid(Grid<GridSpace> grid) throws IllegalStateException
 	{
-		int colors = Color.values().length;
-		int[] portalColors = new int[colors];
-		Direction[][] portalDirections = new Direction[colors][];
-		Grid.Position[] portalLocations = new Grid.Position[colors];
-		
-		List<SlideDoor> slideDoors = new LinkedList<>();
-		
-		for (int row = 0, rows = grid.rows(); row < rows; row++)
+		new GridChecker(grid).checkGrid();
+	}
+	
+	private static final int COLORS = Color.values().length;
+	private int[] portalColors = new int[COLORS];
+	private Direction[][] portalDirections = new Direction[COLORS][];
+	private Grid.Position[] portalLocations = new Grid.Position[COLORS];
+	
+	private LinkedList<SlideDoor> monoSlideDoors = new LinkedList<>();
+	private LinkedList<SlideDoor> upSlideDoors = new LinkedList<>();
+	
+	private Grid<GridSpace> grid;
+	
+	private GridChecker(Grid<GridSpace> grid)
+	{
+		this.grid = grid;
+	}
+	
+	private void checkGrid()
+	{
+		for (int row = 0, rows = this.grid.rows(); row < rows; row++)
 		{
-			for (int col = 0, cols = grid.cols(); col < cols; col++)
+			for (int col = 0, cols = this.grid.cols(); col < cols; col++)
 			{
-				GridSpace cell = grid.get(row, col);
+				GridSpace cell = this.grid.get(row, col);
 				int ID = cell.ID();
 				
-				if (ID == GridSpace.SLIDE_DOOR) checkRail(grid, row, col, (SlideDoor)cell);
-				else if (ID == GridSpace.BOOSTER)
-				{
-					Booster b = (Booster)cell;
-					if (b.rotates) checkBoosterRotates(grid, row, col);
-					else checkBooster(grid, row, col, b.direction);
-				}
-				else if (ID == GridSpace.PORTAl)
-				{
-					Portal tp = (Portal)cell;
-					int color = tp.color.hash;
-					
-					if (++portalColors[color] >= 3)
-						throw new IllegalStateException(new StringBuilder("More than two ").append(tp.color).append(" portals").toString());
-					
-					Direction[] posDirs = GridTraveler.getPossibleDirections(grid, row, col);
-					if (portalDirections[color] == null)
-					{
-						portalDirections[color] = posDirs;
-						portalLocations[color] = new Grid.Position(row, col);
-					}
-					else
-					{
-						Grid.Position pos = portalLocations[color];
-						checkPortals(tp.color, row, col, pos.row, pos.col, posDirs, portalDirections[color]);
-					}
-				}
+				if (ID == GridSpace.SLIDE_DOOR) checkRail((SlideDoor)cell);
+				else if (ID == GridSpace.BOOSTER) checkBooster((Booster)cell);
+				else if (ID == GridSpace.PORTAL) checkPortal((Portal)cell);
 			}
 		}
 		
-		for (Color color : Color.values())
-			if (portalColors[color.hash] == 1)
-				throw new IllegalStateException(new StringBuilder("Found only 1 ").append(color).append(" portal at ").append(portalLocations[color.hash]).toString());
+		checkRailTracks();
+		checkPortalCount();
 	}
 	
 	
-	private static String coords(int row, int col) { return Grid.Position.pair(row, col); }
-	private static String points(Direction dir, boolean rotates)    { return (rotates ? "rotates to point " : "points ").concat(dir.toString()); }
-	
-	private static StringBuilder thingAt (String thing, int row, int col) { return new StringBuilder(thing).append(" at ").append(coords(row, col)).append(' '); }
-	private static StringBuilder thingAtPoints(String thing, int row, int col, Direction dir, boolean rotates) { return thingAt(thing, row, col).append(points(dir, rotates)); }
-	
-	private static <T> boolean contains(T[] things, T thing) { for (T t : things) if (t.equals(thing)) return true; return false; }
-	
-	
-	private static void checkRail(Grid<GridSpace> grid, int row, int col, SlideDoor sd)
+	private void checkRail(SlideDoor sd)
 	{
+		int row = sd.row;
+		int col = sd.col;
 		Direction dir = sd.heading;
 		RailDirection rail = sd.rail;
 		
 		if (sd.up && !rail.contains(dir))
-			throw new IllegalStateException(thingAtPoints("Sliding block", row, col, dir).append(" on a rail that goes ").append(rail.getFirst()).append(" and ").append(rail.getLast()).toString());
+			throw new IllegalStateException(thingAtPoints("Sliding block", row, col, dir, false).append(" on a rail that goes ").append(rail.getFirst()).append(" and ").append(rail.getLast()).toString());
 		
 		Direction first = rail.getFirst();
-		checkRailDirection(grid, row, col, first);
+		checkRailDirection(row, col, first);
 		
 		Direction last = rail.getLast();
-		if (!last.equals(first)) checkRailDirection(grid, row, col, last);
+		if (!last.equals(first)) checkRailDirection(row, col, last);
 		
-		slideDoors.add(sd);
+		if (first == last)
+			this.monoSlideDoors.add(sd);
+		if (sd.up)
+			this.upSlideDoors.add(sd);
 	}
 	
-	private static void checkRailDirection(Grid<GridSpace> grid, int row, int col, Direction dir)
+	private void checkRailDirection(int row, int col, Direction dir)
 	{
 		int r = row + dir.verticalChange();
 		int c = col + dir.horizontalChange();
-		if (!grid.inRange(r, c))
+		if (!this.grid.inRange(r, c))
 			throw new IllegalStateException(thingAtPoints("Rail", row, col, dir, false).append(" out of bounds").toString());
 			
-		GridSpace oCell = grid.get(r, c);
+		GridSpace oCell = this.grid.get(r, c);
 		if (oCell.ID() != GridSpace.SLIDE_DOOR)
 			throw new IllegalStateException(thingAtPoints("Rail", row, col, dir, false).append(" into a non-rail").toString());
 			
@@ -125,27 +109,70 @@ public final class GridChecker
 			throw new IllegalStateException(thingAtPoints("Rail", row, col, dir, false).append(" into rail that does not point ").append(dir.opposite()).toString());
 	}
 	
+	private void checkRailTracks()
+	{
+		
+	}
 	
-	private static void checkBooster         (Grid<GridSpace> grid, int row, int col, Direction dir) { checkBoosterDirection(grid, row, col, dir, false); }
-	private static void checkBoosterRotates  (Grid<GridSpace> grid, int row, int col)                { for (Direction dir : Direction.values()) if (dir.isCardinal()) checkBoosterDirection(grid, row, col, dir, true); }
-	private static void checkBoosterDirection(Grid<GridSpace> grid, int row, int col, Direction dir, boolean rotates)
+	
+	private void checkBooster(Booster b)
+	{
+		int row = b.row;
+		int col = b.col;
+		
+		if (b.rotates)
+		{
+			for (Direction dir : Direction.values())
+				if (dir.isCardinal())
+					checkBoosterDirection(row, col, dir, true);
+		}
+		else
+		{
+			checkBoosterDirection(row, col, b.direction, false);
+		}
+	}
+	
+	private void checkBoosterDirection(int row, int col, Direction dir, boolean rotates)
 	{
 		String name = rotates ? rotatingBoosterName : boosterName;
 		int r = row + dir.verticalChange();
 		int c = col + dir.horizontalChange();
-		if (!grid.inRange(r, c))
+		if (!this.grid.inRange(r, c))
 			throw new IllegalStateException(thingAtPoints(name, row, col, dir, rotates).append(" out of bounds").toString());
 			
-		GridSpace oCell = grid.get(r, c);
+		GridSpace oCell = this.grid.get(r, c);
 		int oID = oCell.ID();
 		if (oID == GridSpace.WALL || oID == GridSpace.BUTTON_DOOR || oID == GridSpace.MOVE_DOOR || oID == GridSpace.SLIDE_DOOR)
 			throw new IllegalStateException(thingAtPoints(name, row, col, dir, rotates).append(" directly into a block that can be solid").toString());
 	}
+	
 	private static final String rotatingBoosterName = "Rotating booster";
 	private static final String boosterName = "Booster";
 	
 	
-	private static void checkPortals(Color color, int entryRow, int entryCol, int exitRow, int exitCol, Direction[] entries, Direction[] exits)
+	private void checkPortal(Portal tp)
+	{
+		int color = tp.color.hash;
+		int row = tp.row;
+		int col = tp.col;
+		
+		if (++this.portalColors[color] >= 3)
+			throw new IllegalStateException(new StringBuilder("More than two ").append(tp.color).append(" this.portals").toString());
+		
+		Direction[] posDirs = GridTraveler.getPossibleDirections(this.grid, row, col);
+		if (this.portalDirections[color] == null)
+		{
+			this.portalDirections[color] = posDirs;
+			this.portalLocations[color] = new Grid.Position(row, col);
+		}
+		else
+		{
+			Grid.Position pos = this.portalLocations[color];
+			checkPortalDirections(tp.color, row, col, pos.row, pos.col, posDirs, this.portalDirections[color]);
+		}
+	}
+	
+	private void checkPortalDirections(Color color, int entryRow, int entryCol, int exitRow, int exitCol, Direction[] entries, Direction[] exits)
 	{
 		for (Direction dir : Direction.values())
 		{
@@ -158,5 +185,22 @@ public final class GridChecker
 			}
 		}
 	}
-	private static String portalMismatch(Color color, Grid.Position entry, Grid.Position exit, Direction dir) { return new StringBuilder().append(color).append(" portals have mismatch in directions: exit portal at ").append(exit).append(" does not allow for entry in portal at ").append(entry).append(" going ").append(dir).toString(); }
+	
+	private void checkPortalCount()
+	{
+		for (Color color : Color.values())
+			if (this.portalColors[color.hash] == 1)
+				throw new IllegalStateException(new StringBuilder("Found only 1 ").append(color).append(" portal at ").append(this.portalLocations[color.hash]).toString());
+	}
+	
+	private static String portalMismatch(Color color, Grid.Position entry, Grid.Position exit, Direction dir) { return new StringBuilder().append(color).append(" this.portals have mismatch in directions: exit portal at ").append(exit).append(" does not allow for entry in portal at ").append(entry).append(" going ").append(dir).toString(); }
+	
+	
+	private static String coords(int row, int col) { return Grid.Position.pair(row, col); }
+	private static String points(Direction dir, boolean rotates) { return (rotates ? "rotates to point " : "points ").concat(dir.toString()); }
+	
+	private static StringBuilder thingAt (String thing, int row, int col) { return new StringBuilder(thing).append(" at ").append(coords(row, col)).append(' '); }
+	private static StringBuilder thingAtPoints(String thing, int row, int col, Direction dir, boolean rotates) { return thingAt(thing, row, col).append(points(dir, rotates)); }
+	
+	private static <T> boolean contains(T[] things, T thing) { for (T t : things) if (t.equals(thing)) return true; return false; }
 }
